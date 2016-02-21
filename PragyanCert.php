f880b86f16533ccb9d7bb2c73633a200f4af9638
keerthana96 <keerthana96@gmail.com>
2016-02-21 16:48:09 +0530

    Incorporated Certificate of Appreciation
    Modified function to work for COA's as well.
    Font changed to PetitaMedium from bediniitalic.

--- PragyanCert.php
+++ PragyanCert.php
@@ -1,5 +1,5 @@
 <?php
-//$type = "Merit" or "Participation" or "Workshop" or "PCA"
+//$type = "Merit" or "Participation" or "Workshop" or "PCA" or "Appreciation"
 //$A4ornot = 1  if A4 else $a$ornot = 0
 //$eventName = name of the event for COP's and COM's. And is the workshop name for Workshops
 //$prize = NULL in the case of COP's and Workshops
@@ -11,11 +11,11 @@ function CertificateWrite($type,$A4ornot,$participantName,$pragyanId,$collegeNam
 
 	putenv('GDFONTPATH=' . realpath('.'));
 	// Set Path to Font File
-	$font_path = 'bediniitalic.ttf';
+	$font_path = 'PetitaMedium.ttf';
 
 	if($A4ornot==1)
 	{
-		// Create image with same size as COP.jpg,COM.jpg and Workshop.jpg
+		// Create plain image with same size as COP.jpg,COM.jpg,Workshop.jpg,COP_CPA.jpg and COA.jpg
 		$image = imagecreatetruecolor(2480, 3508);
 
 		//background color - white
@@ -32,15 +32,15 @@ function CertificateWrite($type,$A4ornot,$participantName,$pragyanId,$collegeNam
 		}
 
 		// Print Text On Image
-		imagettftext($image, 70, 0, 1150, 1660, $black, $font_path, $participantName);
+		imagettftext($image, 60, 0, 1150, 1660, $black, $font_path, $participantName);
 
-		imagettftext($image, 60, 0, 1000, 1780, $black, $font_path, $pragyanId);
+		imagettftext($image, 60, 0, 1050, 1780, $black, $font_path, $pragyanId);
 
-		imagettftext($image, 70, 0, 300, 1900, $black, $font_path, $collegeName);
+		imagettftext($image, 60, 0, 300, 1900, $black, $font_path, $collegeName);
 
-		imagettftext($image, 70, 0, 800, 2020, $black, $font_path, $prize);
+		imagettftext($image, 60, 0, 700, 2020, $black, $font_path, $prize);
 
-		imagettftext($image, 70, 0, 250, 2140, $black, $font_path, $eventName);
+		imagettftext($image, 60, 0, 250, 2140, $black, $font_path, $eventName);
 	}
 
 	else if(strtolower($type)=="participation")
@@ -52,13 +52,13 @@ function CertificateWrite($type,$A4ornot,$participantName,$pragyanId,$collegeNam
 		}
 
 		// Print Text On Image
-		imagettftext($image, 70, 0, 1150, 1660, $black, $font_path, $participantName);
+		imagettftext($image, 60, 0, 1150, 1660, $black, $font_path, $participantName);
 
 		imagettftext($image, 60, 0, 1000, 1780, $black, $font_path, $pragyanId);
 
-		imagettftext($image, 70, 0, 300, 1900, $black, $font_path, $collegeName);
+		imagettftext($image, 60, 0, 300, 1900, $black, $font_path, $collegeName);
 
-		imagettftext($image, 70, 0, 300, 2020, $black, $font_path, $eventName);
+		imagettftext($image, 60, 0, 300, 2020, $black, $font_path, $eventName);
 	}
 
 	else if(strtolower($type)=="workshop")
@@ -70,27 +70,41 @@ function CertificateWrite($type,$A4ornot,$participantName,$pragyanId,$collegeNam
 		}
 
 		// Print Text On Image
-		imagettftext($image, 70, 0, 1150, 1660, $black, $font_path, $participantName);
+		imagettftext($image, 60, 0, 1150, 1660, $black, $font_path, $participantName);
 
 		imagettftext($image, 60, 0, 1000, 1780, $black, $font_path, $pragyanId);
 
-		imagettftext($image, 70, 0, 250, 1900, $black, $font_path, $collegeName);
+		imagettftext($image, 60, 0, 250, 1900, $black, $font_path, $collegeName);
 
-		imagettftext($image, 70, 0, 250, 2020, $black, $font_path, $eventName);
+		imagettftext($image, 60, 0, 250, 2020, $black, $font_path, $eventName);
 	}
 
 	else if(strtolower($type)=="pca")
 	{
 		if($A4ornot==0)
 		{
-			// Create Image From Existing File - Workshop.jpg
+			// Create Image From Existing File - COP_PCA.jpg
 			$image = imagecreatefromjpeg('COP_PCA.jpg');
 		}
 
 		// Print Text On Image
-		imagettftext($image, 70, 0, 550, 1780, $black, $font_path, $participantName);
+		imagettftext($image, 60, 0, 550, 1780, $black, $font_path, $participantName);
 
-		imagettftext($image, 70, 0, 250, 1900, $black, $font_path, $collegeName);
+		imagettftext($image, 60, 0, 250, 1900, $black, $font_path, $collegeName);
+	}
+
+	else if(strtolower($type)=="appreciation")
+	{
+		if($A4ornot==0)
+		{
+			// Create Image From Existing File - COA.jpg
+			$image = imagecreatefromjpeg('COA.jpg');
+		}
+
+		// Print Text On Image
+		imagettftext($image, 60, 0, 500, 1780, $black, $font_path, $participantName);
+
+		imagettftext($image, 60, 0, 800, 1900, $black, $font_path, $pragyanId);
 	}
 
 	header('Content-type: image/jpg');
