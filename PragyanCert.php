39157f74efd6a0315770d6a1575b94fc4097bb57
keerthana96 <keerthana96@gmail.com>
2016-02-21 14:08:11 +0530

    Incorporate PCA certificates
    Fucntion modified to write on PCA certificates as well.

--- PragyanCert.php
+++ PragyanCert.php
@@ -1,5 +1,5 @@
 <?php
-//$type = "Merit" or "Participation" or "Workshop"
+//$type = "Merit" or "Participation" or "Workshop" or "PCA"
 //$A4ornot = 1  if A4 else $a$ornot = 0
 //$eventName = name of the event for COP's and COM's. And is the workshop name for Workshops
 //$prize = NULL in the case of COP's and Workshops
@@ -77,6 +77,20 @@ function CertificateWrite($type,$A4ornot,$participantName,$pragyanId,$collegeNam
 		imagettftext($image, 70, 0, 250, 1900, $black, $font_path, $collegeName);
 
 		imagettftext($image, 70, 0, 250, 2020, $black, $font_path, $eventName);
+	}
+
+	else if(strtolower($type)=="pca")
+	{
+		if($A4ornot==0)
+		{
+			// Create Image From Existing File - Workshop.jpg
+			$image = imagecreatefromjpeg('COP_PCA.jpg');
+		}
+
+		// Print Text On Image
+		imagettftext($image, 70, 0, 550, 1780, $black, $font_path, $participantName);
+
+		imagettftext($image, 70, 0, 250, 1900, $black, $font_path, $collegeName);
 	}
 
 	header('Content-type: image/jpg');
